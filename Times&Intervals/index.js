332cd7492ea2e53b5cbc3d65da9625d935d511e7
Shayab Rahman <shayabrahman354@gmail.com>
2026-03-18 18:52:44 +0530

    commit

---
+++ Times&Intervals/index.js
@@ -0,0 +1,28 @@
+// setTimeout(function(){
+//     console.log("shayab");
+// }, 3000);
+
+
+
+// setInterval(function(){
+//     console.log("shayab");
+// }, 3000);
+
+// let t_out = setTimeout(function(){
+//     console.log("shayab");
+// }, 3000);
+
+// clearTimeout(t_out);
+
+// let count = 10;
+
+// let interval = setInterval(function(){
+//     if(count>=0){
+        
+//         console.log(count);
+//         count--;
+//     }
+//     else
+//         clearInterval(interval);
+// },1000);
+
