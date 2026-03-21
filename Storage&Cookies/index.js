da762c7f942503412c12699809ef51f9e267943e
Shayab Rahman <shayabrahman354@gmail.com>
2026-03-21 11:16:15 +0530

    commit

---
+++ Storage&Cookies/index.js
@@ -0,0 +1,21 @@
+// localStorage.setItem("name", "shayab");
+
+// let val = localStorage.getItem("name");
+
+// localStorage.removeItem("name");
+
+// localStorage.setItem("name", "Rahman");
+
+// localStorage.setItem("friends", ["shayab", "rahman", "heheheh"]);
+
+// let fr = JSON.parse(localStorag.getItem("friends"));
+// console.log(fr);
+
+// if(window.matchMedia('(prefers-color-scheme:dark)').matches){
+//     document.body.classList.add("dark");
+// }
+// else
+//      document.body.classList.add("dark");
+
+
+
