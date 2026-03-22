aae42cd8430a24578b2197e402392eacc428f42c
Shayab Rahman <shayabrahman354@gmail.com>
2026-03-22 11:34:35 +0530

    commit

---
+++ DOM/basic.js
@@ -0,0 +1,80 @@
+// let h1 = document.querySelector("h1");
+// h1.innerHTML = "<i>Shayab Rahman</>";
+// h1.hidden = true;
+
+// let a = document.querySelector("a");
+// a.setAttribute("href", "https://www.google.com");
+
+// let img = document.querySelector("img");
+// img.setAttribute("src" , "https://unsplash.com/photos/a-young-girl-eating-a-fresh-peach-outdoors-BvZYHz9TeCk");
+
+// let a = document.querySelector("a");
+// console.log(a.getAttribute("href"));
+
+// let a = document.querySelector("a");
+// a.removeAttribute("href");
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "shayab rahman";
+// document.body.prepend(h1);
+
+// let h1 = document.querySelector("h1");
+// h1.style.color = "red";
+// h1.style.backgroundColor = "blue";
+// h1.style.fontFamily = "Gilroy";
+
+// let a = document.getElementsByClassName("h");
+// console.log(a);
+
+// let heading = document.querySelector("#heading");
+// heading.textContent = "Shayab Rahman";
+// let list = document.querySelectorAll("li");
+// list.forEach(function(val){
+//     console.log(val.textContent);
+// });
+// let list = document.querySelectorAll("li");
+// for(let i = 0; i<list.length; i++){
+//     console.log(list[i].textContent);
+// }
+
+// let p = document.querySelector("p");
+// p.innerHTML = "<b>Updated</b> by JavaScript";
+
+// let img = document.querySelector("img");
+// // console.log(img.src);
+// console.log(img.getAttribute("src"));
+
+// let a = document.querySelector("a");
+// a.href = "https://www.sheryians.com";
+
+// let div = document.querySelector("div");
+// div.setAttribute("title", "some info");
+
+// let btn = document.querySelector("button");
+// btn.removeAttribute("disabled");
+
+// let ul = document.querySelector("ul");
+// let li = document.createElement("li");
+
+// li.textContent = "new text";
+// ul.append(li);
+
+// let img = document.createElement("img");
+// img.classList.add("placeholder");
+// img.setAttribute("src", "https://t4.ftcdn.net/jpg/06/57/37/01/360_F_657370150_pdNeG5pjI976ZasVbKN9VqH1rfoykdYU.jpg");
+// document.querySelector("div").prepend(img);
+
+// let ul = document.querySelector("ul");
+// let li = document.querySelector("li");
+// ul.removeChild(li);
+
+// let li = document.querySelectorAll("ul li:nth-child(2n)");
+
+// li.forEach(function(ele){
+//     ele.classList.add("highlight");
+// });
+
+// let p = document.querySelectorAll("p");
+// p.forEach(function(elem){
+//     elem.style.fontSize = "18px";
+// });
