ac4c8c396401db950189ccdfbf167713cfb33ca8
Shayab Rahman <shayabrahman354@gmail.com>
2026-03-28 14:05:55 +0530

    commit

---
+++ DOM_1/script.js
@@ -0,0 +1,10 @@
+// let mydiv = document.querySelector('#mydiv');
+
+// let newElement = document.createElement('span');
+// newElement.textContent =" shayab";
+
+// mydiv.insertAdjacentElement('beforeend', newElement);
+
+let parent = document.querySelector("#mydiv");
+let child = document.querySelector("#para");
+parent.removeChild(child);
