159fb9178fa602b101c6e26ba34c5a9271fbf3df
Shayab Rahman <shayabrahman354@gmail.com>
2026-03-29 11:01:16 +0530

    commit

---
+++ DOM_2/script.js
@@ -0,0 +1,52 @@
+// let a = document.querySelector("h1");
+// console.log(a);
+
+// let a = document.querySelector("h1");
+// a.innerHTML = "Shayab";
+
+// let a = document.querySelector("h1");
+// a.style.color = "red";
+// a.style.backgroundColor = "black";
+
+// let a = document.querySelector("h1");
+// a.addEventListener("click",function(){
+//     a.innerHTML= "changed";
+//     a.style.color = "red";
+//     a.style.backgroundColor = "pink";
+//     a.style.padding = "50px";
+//     a.style.borderRadius = "50%";
+    
+// });
+
+// let bulb = document.querySelector("#bulb");
+// let btn = document.querySelector("button");
+
+// let flag = 0;
+
+// btn.addEventListener("click", function(){
+//     if(flag == 0){
+//         bulb.style.backgroundColor = "yellow";
+//         console.log("ON");
+//         flag = 1;
+        
+//     }
+//     else{
+//         bulb.style.backgroundColor = "transparent";
+//         console.log("OFF");
+//         flag = 0;
+//     }
+   
+// });
+
+// let h = document.querySelectorAll("h1");
+// console.log(h);
+// h.forEach(function(val){
+//     console.log(val);
+// });
+
+// let h = document.getElementById(`box`);
+// console.log(h);
+
+// let box = document.querySelector("#box");
+// box.innerHTML = "<h1>shayab</h1>";
+// box.textContent = "<h1>rahman</h1>";
