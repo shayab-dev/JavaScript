1dd9afa57ed2281064375c4ef1b4de1b9ed76383
Shayab Rahman <shayabrahman354@gmail.com>
2026-04-21 00:46:45 +0530

    commit

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -9,6 +9,7 @@ function validateName(){
 
   if(name.length == 0){
     nameError.innerHTML = 'Name is reruired';
+    nameError.style.color = 'red';
     return false;
   }
   if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
@@ -16,5 +17,29 @@ function validateName(){
     return false;
   }
   nameError.innerHTML =  'valid';
+  nameError.style.color = 'seagreen';
   return true;
+}
+
+function validatePhone(){
+  let phone = document.getElementById('contact-phone').value;
+
+  if(phone.length == 0){
+    phoneError.innerHTML = 'Invalid';
+    phoneError.style.color = 'red';
+    return false;
+  }
+  if(phone.lenght !== 10){
+    phoneError.innerHTML = 'Phone no should be 10 digits';
+    return false;
+  }
+  if(!phone.match(/^[0-9]{10}$/)){
+    phoneError.innerHTML = 'Only digits please';
+    return false;
+  }
+
+  phoneError.innerHTML = 'ok';
+  phoneError.style.color = 'seagreen';
+  return true;
+  
 }
