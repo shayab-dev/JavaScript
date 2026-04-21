21b80df473b2f0ecda72d1556c452d0af4a2857c
Shayab Rahman <shayabrahman354@gmail.com>
2026-04-22 00:05:40 +0530

    commit

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -5,41 +5,80 @@ let messageError = document.getElementById('message-error');
 let submitError = document.getElementById('submit-error');
 
 function validateName(){
-  let name = document.getElementById('contact-name').value;
+  let name = document.getElementById('contact-name').value.trim();
 
   if(name.length == 0){
     nameError.innerHTML = 'Name is reruired';
-    nameError.style.color = 'red';
     return false;
   }
   if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
     nameError.innerHTML = 'Write full name';
+    nameError.style.color = 'red';
     return false;
   }
-  nameError.innerHTML =  'valid';
+  nameError.innerHTML =  'ok';
   nameError.style.color = 'seagreen';
   return true;
 }
 
 function validatePhone(){
-  let phone = document.getElementById('contact-phone').value;
+    let phone = document.getElementById('contact-phone').value.trim();
 
-  if(phone.length == 0){
-    phoneError.innerHTML = 'Invalid';
-    phoneError.style.color = 'red';
-    return false;
-  }
-  if(phone.lenght !== 10){
-    phoneError.innerHTML = 'Phone no should be 10 digits';
-    return false;
-  }
-  if(!phone.match(/^[0-9]{10}$/)){
-    phoneError.innerHTML = 'Only digits please';
+    if(phone.length === 0){
+        phoneError.innerHTML = 'Invalid';
+        return false;
+    }
+
+    if(!/^\d{10}$/.test(phone)){
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        return false;
+    }
+
+    phoneError.innerHTML = 'ok';
+    phoneError.style.color = 'seagreen';
+    return true;
+}
+
+function validateEmail(){
+  let email = document.getElementById('contact-email').value.trim();
+
+  if(email.length == 0){
+    emailError.innerHTML = 'Email is required';
     return false;
   }
 
-  phoneError.innerHTML = 'ok';
-  phoneError.style.color = 'seagreen';
+ if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+    emailError.innerHTML = 'Invalid';
+    return false;
+ }
+
+  emailError.innerHTML = 'ok';
+  emailError.style.color = 'seagreen';
   return true;
-  
+}
+
+function validateMessage(){
+  let message = document.getElementById('contact-message').value;
+
+  let required = 30;
+  let left = required - message.length;
+
+  if(left > 0){
+    messageError.innerHTML = left + 'more characters required';
+    return false;
+  }
+
+  messageError.innerHTML = 'ok';
+  messageError.style.color = 'seagreen';
+}
+
+function validateForm(){
+  if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage() ){
+    submitError.style.display = 'block';
+    submitError.innerHTML = 'Please fix the error to submit';
+    setTimeout(function(){submitError.style.display = 'none';},3000);
+    return false;
+  }
+
+  return true;
 }
