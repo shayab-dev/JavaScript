40c09ccb950277711f6790d477bfab3d0909e253
Shayab Rahman <shayabrahman354@gmail.com>
2026-04-24 20:04:44 +0530

    commit

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,28 +1,6 @@
 const apiKey = '8bc18b8b4affe9f661740928ae9c1715';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
-
-// const searchBox = document.querySelector('.search input');
-
-// const searchBtn = document.querySelector('.search button');
-
-
-// async function checkWeather(city){
-//   const response = await fetch(apiUrl + encodeURIComponent(city) + `&appid=${apiKey}`);
-//   let data = await response.json();
-
-//   console.log(data);
-
-//   document.querySelector('.city').innerHTML = data.name;
-//   document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°c';
-//   document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
-//   document.querySelector('.wind').innerHTML = data.wind.speed + 'km/h';
-// }
-
-// searchBtn.addEventListner('click', ()=> {
-//   checkWeather(searchBox.value.trim());
-// });
-
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
 const weatherIcon = document.querySelector('.weather-icon');
